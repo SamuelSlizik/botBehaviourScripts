0050ded26798fc2030f5e6e2997477449701425e
SamuelSlizik <samuel.slizik@binconf.com>
2023-07-12 18:56:17 +0200

    Update main.py

--- PythonScripts/behaviourManagerScript/main.py
+++ PythonScripts/behaviourManagerScript/main.py
@@ -2,4 +2,8 @@ import subprocess
 
 command = ["C:\\sikulixide.jar", "-r", "C:\BotBehaviourScripts\checkTodaysFolder.sikuli"]
 
+subprocess.run(command, check=True)
+
 print("HELP")
+
+input("Press Enter to continue...")
