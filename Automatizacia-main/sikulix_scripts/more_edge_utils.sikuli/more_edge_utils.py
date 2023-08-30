6359ee6334286a94f3af4262f18e1208259e92fe
SamuelSlizik <129870569+SamuelSlizik@users.noreply.github.com>
2023-08-30 06:37:17 -0700

    updated procrastination to be more dynamic

--- Automatizacia-main/sikulix_scripts/more_edge_utils.sikuli/more_edge_utils.py
+++ Automatizacia-main/sikulix_scripts/more_edge_utils.sikuli/more_edge_utils.py
@@ -3,7 +3,22 @@ import logger
 import random
 
 
-def edge_scroll_images(time):
+def start_edge_scroll_images():
+    try:
+        wait("1690286144726.png")
+        click("1690286144726.png")
+        wait("1690286179068.png")
+        logger.log("info", "Finished scrolling images", "success")
+    except FindFailed as ex:
+        logger.log("error", "Error while scrolling images", "failure", ex)
+
+
+def edge_scroll_images():
+    wheel(WHEEL_DOWN, 1)
+            wait(5)
+    
+
+def old_edge_scroll_images(time):
     try:
         convertedTime = float(time)
         wait("1690286144726.png")
@@ -12,12 +27,32 @@ def edge_scroll_images(time):
         for i in range(int(convertedTime / 5)):
             wheel(WHEEL_DOWN, 1)
             wait(5)
-        logger.log("info", "Finished scrolling images", "success")
+        logger.log("info", "Finished start scrolling images", "success")
     except FindFailed as ex:
         logger.log("error", "Error while scrolling images", "failure", ex)
 
 
-def edge_watch_youtube_shorts(time):
+def start_edge_watch_youtube():
+    try:
+        wait("1690290587338.png")
+        click("1690290587338.png")
+        wait("1690290670091.png")
+        logger.log("info", "Finished watching youtube shorts", "success")
+    except Exception as ex:
+        logger.log("error", "Error while watching youtube shorts", "failure", ex)
+
+
+def edge_watch_youtube_shorts():
+    try:
+        watch = random.uniform(10.0, 20.0)
+        wait(watch)
+        click("1690290670091.png")    
+        logger.log("info", "Finished watching youtube shorts", "success")
+    except Exception as ex:
+        logger.log("error", "Error while watching youtube shorts", "failure", ex)
+
+
+def old_edge_watch_youtube_shorts(time):
     try:
         convertedTime = float(time)
         wait("1690290587338.png")
@@ -41,9 +76,9 @@ if __name__ == "__main__":
 
     function_name = sys.argv[1]
 
-    if function_name == "edge_scroll_images":
+    if function_name == "old_edge_scroll_images":
         time = sys.argv[2]
-        edge_scroll_images(time)
-    elif function_name == "edge_watch_youtube_shorts":
+        old_edge_scroll_images(time)
+    elif function_name == "old_edge_watch_youtube_shorts":
         time = sys.argv[2]
-        edge_watch_youtube_shorts(time)
+        old_edge_watch_youtube_shorts(time)
